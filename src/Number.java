ce95208a0ba716c0fd2b520b87465bb114cdd5d5
mezd10 <sn_kh5@mail.ru>
2017-03-05 21:10:11 +0300

    plus, minus

--- src/Number.java
+++ src/Number.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Number {
 
@@ -18,46 +20,157 @@ public final class Number {
         this.numberUnion = numberUnion;
     }
 
+    public Number(String number) throws NumberFormatException {
+        List<Integer> result = new ArrayList<>();
+        int before = 0;
+        int after = 0;
+        int i = 0;
+        Pattern p = Pattern.compile("((^-)|(^))((\\d+$)|(\\d+\\.\\d+$))");
+        Matcher m = p.matcher(number);
+        if (!m.find()) throw new NumberFormatException();
+        if (m.group().charAt(0) == '-') {
+            i = 2;
+            result.add(-1 * (m.group().codePointAt(1) - '0'));
+            before++;
+        }
+        for (; i < m.group().length(); i++) {
+            if (m.group().charAt(i) == '.') continue;
+            if (after != 0 || (i != 0 && m.group().charAt(i - 1) == '.')) {
+                after++;
+                result.add(m.group().codePointAt(i) - '0');
+            } else {
+                before++;
+                result.add(m.group().codePointAt(i) - '0');
+            }
+        }
+        this.before = before;
+        this.after = after;
+        this.numberUnion = result;
+    }
+
     public Number plus(Number other) {
 
         List<Integer> result = new ArrayList<>();
         int maxAfter = Math.max(after, other.after);
+        int sign = 0;
 
-        for (int i = 0; i < maxAfter - after; i++) {
-            numberUnion.add(0);
-        }
+        if ((numberUnion.get(0) <= 0 && other.numberUnion.get(0) <= 0) || (numberUnion.get(0) >= 0 && other.numberUnion.get(0) >= 0)) {
 
-        for (int i = 0; i < maxAfter - other.after; i++) {
-            other.numberUnion.add(0);
-        }
-
-        int maxBefore = Math.max(before, other.before);
-
-        for (int i = 0; i < maxBefore - before; i++) {
-            numberUnion.add(0, 0);
-        }
-
-        for (int i = 0; i < maxBefore - other.before; i++) {
-            other.numberUnion.add(0, 0);
-        }
-
-        int excess = 0;
-
-        for (int i = maxBefore + maxAfter - 1; i >= 0; i--) {
-            result.add(0, (numberUnion.get(i) + other.numberUnion.get(i) + excess) % 10);
-
-            if (numberUnion.get(i) + other.numberUnion.get(i) + excess > 9) {
-                excess = 1;
-            } else {
-                excess = 0;
+            if (numberUnion.get(0) < 0) {
+                sign = 1;
+                int per = numberUnion.get(0);
+                per *= -1;
+                numberUnion.remove(0);
+                numberUnion.add(0, per);
+                per = other.numberUnion.get(0);
+                per *= -1;
+                other.numberUnion.remove(0);
+                other.numberUnion.add(0, per);
             }
-        }
 
-        if (excess > 0) {
-            result.add(0, 1);
+            for (int i = 0; i < maxAfter - after; i++) {
+                numberUnion.add(0);
+            }
+
+            for (int i = 0; i < maxAfter - other.after; i++) {
+                other.numberUnion.add(0);
+            }
+
+            int maxBefore = Math.max(before, other.before);
+
+            for (int i = 0; i < maxBefore - before; i++) {
+                numberUnion.add(0, 0);
+            }
+
+            for (int i = 0; i < maxBefore - other.before; i++) {
+                other.numberUnion.add(0, 0);
+            }
+
+            int excess = 0;
+
+            for (int i = maxBefore + maxAfter - 1; i >= 0; i--) {
+                result.add(0, (numberUnion.get(i) + other.numberUnion.get(i) + excess) % 10);
+
+                if (numberUnion.get(i) + other.numberUnion.get(i) + excess > 9) {
+                    excess = 1;
+                } else {
+                    excess = 0;
+                }
+            }
+
+            if (excess > 0) {
+                result.add(0, 1);
+            }
+            result.add(0, sign);
+
+            return new Number(result.size() - maxAfter, maxAfter, result);
+        } else {
+
+            for (int i = 0; i < maxAfter - after; i++) {
+                numberUnion.add(0);
+            }
+
+            for (int i = 0; i < maxAfter - other.after; i++) {
+                other.numberUnion.add(0);
+            }
+
+            int maxBefore = Math.max(before, other.before);
+
+            for (int i = 0; i < maxBefore - before; i++) {
+                numberUnion.add(0, 0);
+            }
+
+            for (int i = 0; i < maxBefore - other.before; i++) {
+                other.numberUnion.add(0, 0);
+            }
+
+            List<Integer> maxNumber = maxUnionNumber(this, other);
+            List<Integer> minNumber;
+
+            if (maxNumber.equals(this.numberUnion)) {
+                minNumber = other.numberUnion;
+            } else {
+                minNumber = numberUnion;
+            }
+            if (maxNumber.get(0) < 0) {
+                sign = 1;
+            }
+
+            int firstElement = maxNumber.get(0);
+            if (firstElement < 0) {
+                int per = maxNumber.get(0);
+                per *= -1;
+                maxNumber.remove(0);
+                maxNumber.add(0, per);
+            }
+
+
+            if (minNumber.get(0) < 0) {
+                int per = minNumber.get(0);
+                per *= -1;
+                minNumber.remove(0);
+                minNumber.add(0, per);
+            }
+
+            Collections.reverse(maxNumber);
+            Collections.reverse(minNumber);
+
+            int shortcoming = 0;
+
+            for (int i = 0; i < maxNumber.size(); i++) {
+                if (maxNumber.get(i) - minNumber.get(i) - shortcoming < 0) {
+                    result.add(0, maxNumber.get(i) - minNumber.get(i) - shortcoming + 10);
+                    shortcoming = 1;
+                } else {
+                    result.add(0, maxNumber.get(i) - minNumber.get(i) - shortcoming);
+                    shortcoming = 0;
+                }
+            }
+            result.add(0, sign);
+            return new Number(result.size() - maxAfter, maxAfter, result);
         }
-        return new Number(result.size() - maxAfter, maxAfter, result);
     }
+
 
     private List<Integer> maxUnionNumber(Number numb1,
                                          Number numb2) {
@@ -67,7 +180,10 @@ public final class Number {
         if (numb1.equals(numb2)) return numberUnion;
         if (numb1.before > numb2.before) return numb1.numberUnion;
         if (numb1.before < numb2.before) return numb2.numberUnion;
-        else {
+        if (numb1.before == numb2.before && Math.abs(numb1.numberUnion.get(0)) == Math.abs(numb2.numberUnion.get(0))) {
+            if (numb1.numberUnion.get(0) >= 0) return numb1.numberUnion;
+            else return numb2.numberUnion;
+        } else {
             list = numb1.numberUnion;
             otherList = numb2.numberUnion;
             int max = Math.max(list.size(), otherList.size());
@@ -78,71 +194,114 @@ public final class Number {
                 otherList.add(0);
             }
             int i = 0;
+            int sign = 0;
+            int sign1 = 0;
+            if (list.get(0) < 0) {
+                sign = 1;
+                int per = list.get(0);
+                per *= -1;
+                list.remove(0);
+                list.add(0, per);
+            }
+            if (otherList.get(0) < 0) {
+                sign1 = 1;
+                int per = otherList.get(0);
+                per *= -1;
+                otherList.remove(0);
+                otherList.add(0, per);
+            }
             while (true) {
-                if (list.get(i) > otherList.get(i)) return list;
-                if (list.get(i) < otherList.get(i)) return otherList;
+                if (list.get(i) > otherList.get(i)) {
+                    if (sign == 1) {
+                        int per = list.get(0);
+                        per *= -1;
+                        list.remove(0);
+                        list.add(0, per);
+                    }
+                    return list;
+                }
+                if (list.get(i) < otherList.get(i)) {
+                    if (sign1 == 1) {
+                        int per = otherList.get(0);
+                        per *= -1;
+                        otherList.remove(0);
+                        otherList.add(0, per);
+                    }
+                    return otherList;
+                }
                 i++;
-
             }
         }
     }
 
     public Number minus(Number other) {
+        int sign = 0;
+        if (numberUnion.get(0) >= 0 && other.numberUnion.get(0) >= 0) {
 
-        List<Integer> result = new ArrayList<>();
-        int maxAfter = Math.max(after, other.after);
+            List<Integer> result = new ArrayList<>();
+            int maxAfter = Math.max(after, other.after);
 
-        for (int i = 0; i < maxAfter - after; i++) {
-            numberUnion.add(0);
-        }
-
-        for (int i = 0; i < maxAfter - other.after; i++) {
-            other.numberUnion.add(0);
-        }
-
-        int maxBefore = Math.max(before, other.before);
-
-        for (int i = 0; i < maxBefore - before; i++) {
-            numberUnion.add(0, 0);
-        }
-
-        for (int i = 0; i < maxBefore - other.before; i++) {
-            other.numberUnion.add(0, 0);
-        }
-
-        List<Integer> maxNumber = maxUnionNumber(this, other);
-        List<Integer> minNumber;
-
-        if (maxNumber.equals(this.numberUnion)) {
-            minNumber = other.numberUnion;
-        } else {
-            minNumber = numberUnion;
-        }
-
-        Collections.reverse(maxNumber);
-        Collections.reverse(minNumber);
-
-        int shortcoming = 0;
-
-        for (int i = 0; i < maxNumber.size(); i++) {
-            if (maxNumber.get(i) - minNumber.get(i) - shortcoming < 0) {
-                result.add(0, maxNumber.get(i) - minNumber.get(i) - shortcoming + 10);
-                shortcoming = 1;
-            } else {
-                result.add(0, maxNumber.get(i) - minNumber.get(i) - shortcoming);
-                shortcoming = 0;
+            for (int i = 0; i < maxAfter - after; i++) {
+                numberUnion.add(0);
             }
+
+            for (int i = 0; i < maxAfter - other.after; i++) {
+                other.numberUnion.add(0);
+            }
+
+            int maxBefore = Math.max(before, other.before);
+
+            for (int i = 0; i < maxBefore - before; i++) {
+                numberUnion.add(0, 0);
+            }
+
+            for (int i = 0; i < maxBefore - other.before; i++) {
+                other.numberUnion.add(0, 0);
+            }
+
+            List<Integer> maxNumber = maxUnionNumber(this, other);
+            List<Integer> minNumber;
+
+            if (maxNumber.equals(this.numberUnion)) {
+                minNumber = other.numberUnion;
+            } else {
+                minNumber = numberUnion;
+                sign = 1;
+            }
+
+            Collections.reverse(maxNumber);
+            Collections.reverse(minNumber);
+
+            int shortcoming = 0;
+
+            for (int i = 0; i < maxNumber.size(); i++) {
+                if (maxNumber.get(i) - minNumber.get(i) - shortcoming < 0) {
+                    result.add(0, maxNumber.get(i) - minNumber.get(i) - shortcoming + 10);
+                    shortcoming = 1;
+                } else {
+                    result.add(0, maxNumber.get(i) - minNumber.get(i) - shortcoming);
+                    shortcoming = 0;
+                }
+            }
+            result.add(0, sign);
+            return new Number(result.size() - maxAfter, maxAfter, result);
+        } else {
+            int per = other.numberUnion.get(0);
+            per *= -1;
+            other.numberUnion.remove(0);
+            other.numberUnion.add(0, per);
+            Number number = this;
+            Number number1 = other;
+            return number.plus(number1);
         }
-        if (result.get(0) == 0) {
-            result.remove(0);
-        }
-        return new Number(result.size() - maxAfter, maxAfter, result);
     }
+
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < before; i++) {
+        if (numberUnion.get(0) == 1) result.append("-");
+        for (int i = 1; i < before; i++) {
             result.append(numberUnion.get(i));
         }
         if (after != 0) result.append(".");
--- test/NumberTest.java
+++ test/NumberTest.java
@@ -9,26 +9,30 @@ public class NumberTest {
 
     @Test
     public void testToString() throws Exception {
-        List<Integer> numberUnion = Arrays.asList(1, 8, 9, 2, 1);
-        assertEquals("189.21", new Number(3, 2, numberUnion).toString());
+        List<Integer> numberUnion = Arrays.asList(0, -1, 8, 9, 2, 1);
+        assertEquals("-189.21", new Number(4, 2, numberUnion).toString());
     }
 
     @Test
     public void plus() {
         List<Integer> numberUnion = new ArrayList<>();
         List<Integer> otherNumberUnion = new ArrayList<>();
-        Collections.addAll(numberUnion, 9, 1, 1);
-        Collections.addAll(otherNumberUnion, -1, -1);
-        assertEquals("90.0", (new Number(2, 1, numberUnion).plus(new Number(1, 1, otherNumberUnion))).toString());
+        Collections.addAll(numberUnion, -1, 3, 9);
+        Collections.addAll(otherNumberUnion, 1, 3, 9);
+        assertEquals("00.0", (new Number(2, 1, numberUnion).plus(new Number(2, 1, otherNumberUnion))).toString());
+        assertEquals("110.50", (new Number("55.45").plus(new Number("55.05"))).toString());
+        assertEquals("1118888.9999",(new Number("1000000.101").plus(new Number("118888.8989"))).toString());
+        assertEquals("-30.9", (new Number("-15.6").plus(new Number("-15.3"))).toString());
     }
 
     @Test
     public void minus() {
         List<Integer> numberUnion = new ArrayList<>();
         List<Integer> otherNumberUnion = new ArrayList<>();
-        Collections.addAll(numberUnion, 1, 9, 9);
-        Collections.addAll(otherNumberUnion, 1, 0, 3);
-        assertEquals("96", (new Number(3, 0, numberUnion).minus(new Number(3, 0, otherNumberUnion))).toString());
+        Collections.addAll(numberUnion, -1, 3);
+        Collections.addAll(otherNumberUnion, -1, 5);
+        assertEquals("0.2", (new Number(1, 1, numberUnion).minus(new Number(1, 1, otherNumberUnion))).toString());
+        assertEquals("-3.0", (new Number("5.9").minus(new Number("8.9"))).toString());
     }
 
 }
