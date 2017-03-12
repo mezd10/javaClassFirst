ac1475df9a5056ba52a03b8e830923487fe4831e
mezd10 <sn_kh5@mail.ru>
2017-03-12 13:56:35 +0300

    all

--- src/FractionalNumber.java
+++ src/FractionalNumber.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class Number {
+public final class FractionalNumber {
 
     private final int before;
 
@@ -12,15 +12,19 @@ public final class Number {
 
     private final List<Integer> numberUnion;
 
-    public Number(int before, int after,
-                  List<Integer> numberUnion) {
+    private int sign;
+
+    private int signNull;
+
+    public FractionalNumber(int before, int after,
+                            List<Integer> numberUnion) {
 
         this.before = before;
         this.after = after;
         this.numberUnion = numberUnion;
     }
 
-    public Number(String number) throws NumberFormatException {
+    public FractionalNumber(String number) throws NumberFormatException {
         List<Integer> result = new ArrayList<>();
         int before = 0;
         int after = 0;
@@ -29,10 +33,17 @@ public final class Number {
         Matcher m = p.matcher(number);
         if (!m.find()) throw new NumberFormatException();
         if (m.group().charAt(0) == '-') {
+            sign = 1;
+            if(m.group().charAt(1) == '0'){
+                signNull = 1;
+            }
             i = 2;
             result.add(-1 * (m.group().codePointAt(1) - '0'));
             before++;
         }
+        else if (m.group().charAt(0) == '0')
+                signNull = 0;
+
         for (; i < m.group().length(); i++) {
             if (m.group().charAt(i) == '.') continue;
             if (after != 0 || (i != 0 && m.group().charAt(i - 1) == '.')) {
@@ -48,13 +59,13 @@ public final class Number {
         this.numberUnion = result;
     }
 
-    public Number plus(Number other) {
+    public FractionalNumber plus(FractionalNumber other) {
 
         List<Integer> result = new ArrayList<>();
         int maxAfter = Math.max(after, other.after);
-        int sign = 0;
 
-        if ((numberUnion.get(0) <= 0 && other.numberUnion.get(0) <= 0) || (numberUnion.get(0) >= 0 && other.numberUnion.get(0) >= 0)) {
+        if (((numberUnion.get(0) <= 0 && other.numberUnion.get(0) <= 0) || (numberUnion.get(0) >= 0 && other.numberUnion.get(0) >= 0))
+                && (signNull != 1 || other.signNull != 0) && (signNull != 0 || other.signNull != 1)){
 
             if (numberUnion.get(0) < 0) {
                 sign = 1;
@@ -101,9 +112,9 @@ public final class Number {
             if (excess > 0) {
                 result.add(0, 1);
             }
-            result.add(0, sign);
+            result.add(0,sign);
 
-            return new Number(result.size() - maxAfter, maxAfter, result);
+            return new FractionalNumber(result.size() - maxAfter, maxAfter, result);
         } else {
 
             for (int i = 0; i < maxAfter - after; i++) {
@@ -144,7 +155,6 @@ public final class Number {
                 maxNumber.add(0, per);
             }
 
-
             if (minNumber.get(0) < 0) {
                 int per = minNumber.get(0);
                 per *= -1;
@@ -166,14 +176,15 @@ public final class Number {
                     shortcoming = 0;
                 }
             }
-            result.add(0, sign);
-            return new Number(result.size() - maxAfter, maxAfter, result);
+            result.add(0,sign);
+
+            return new FractionalNumber(result.size() - maxAfter, maxAfter, result);
         }
     }
 
 
-    private List<Integer> maxUnionNumber(Number numb1,
-                                         Number numb2) {
+    private List<Integer> maxUnionNumber(FractionalNumber numb1,
+                                         FractionalNumber numb2) {
         List<Integer> list;
         List<Integer> otherList;
 
@@ -234,9 +245,10 @@ public final class Number {
         }
     }
 
-    public Number minus(Number other) {
-        int sign = 0;
-        if (numberUnion.get(0) >= 0 && other.numberUnion.get(0) >= 0) {
+    public FractionalNumber minus(FractionalNumber other) {
+
+        if ((numberUnion.get(0) >= 0 && other.numberUnion.get(0) >= 0) && (signNull != 1 || other.signNull != 0)
+                && (signNull != 0 || other.signNull != 1)) {
 
             List<Integer> result = new ArrayList<>();
             int maxAfter = Math.max(after, other.after);
@@ -283,19 +295,133 @@ public final class Number {
                     shortcoming = 0;
                 }
             }
-            result.add(0, sign);
-            return new Number(result.size() - maxAfter, maxAfter, result);
+            result.add(0,sign);
+
+            return new FractionalNumber(result.size() - maxAfter, maxAfter, result);
         } else {
+            if (signNull == 1 && other.signNull == 0) {
+                signNull = 2;
+                other.signNull = 2;
+            } else if(signNull == 0 && other.signNull == 1){
+                signNull = 2;
+                other.signNull = 2;
+            }
             int per = other.numberUnion.get(0);
             per *= -1;
             other.numberUnion.remove(0);
             other.numberUnion.add(0, per);
-            Number number = this;
-            Number number1 = other;
+            FractionalNumber number = this;
+            FractionalNumber number1 = other;
             return number.plus(number1);
         }
     }
 
+    public FractionalNumber multiplication(FractionalNumber other) {
+
+        if (numberUnion.get(0) < 0 && other.numberUnion.get(0) > 0) {
+            sign = 1;
+            int per = numberUnion.get(0);
+            per *= -1;
+            numberUnion.remove(0);
+            numberUnion.add(0, per);
+        }
+
+        if (numberUnion.get(0) > 0 && other.numberUnion.get(0) < 0) {
+            sign = 1;
+            int per = other.numberUnion.get(0);
+            per *= -1;
+            other.numberUnion.remove(0);
+            other.numberUnion.add(0, per);
+        }
+
+        if (numberUnion.get(0) < 0 && other.numberUnion.get(0) < 0) {
+            int per = numberUnion.get(0);
+            per *= -1;
+            numberUnion.remove(0);
+            numberUnion.add(0, per);
+            per = other.numberUnion.get(0);
+            per *= -1;
+            other.numberUnion.remove(0);
+            other.numberUnion.add(0, per);
+            sign = 0;
+        }
+
+        if (signNull == 1 && other.signNull == 1) sign = 0;
+        if ((signNull == 1 && other.signNull == 0) || (signNull == 0 && other.signNull == 1)) sign = 1;
+
+        List<Integer> result = new ArrayList<>();
+        Collections.reverse(numberUnion);
+        Collections.reverse(other.numberUnion);
+        int excess;
+        int rez;
+        for (int i = 0; i < numberUnion.size(); i++) {
+            excess = 0;
+            for (int j = 0; j < other.numberUnion.size(); j++) {
+                if (i + j > result.size() - 1) {
+                    rez = numberUnion.get(i) * other.numberUnion.get(j) + excess;
+                    result.add(rez % 10);
+                } else {
+                    rez = numberUnion.get(i) * other.numberUnion.get(j) + excess + result.get(i + j);
+                    result.set(i + j, rez % 10);
+                }
+                excess = rez / 10;
+            }
+            if (i + other.numberUnion.size() > result.size() - 1) {
+                result.add(excess);
+            } else {
+                result.set(i + other.numberUnion.size(), excess);
+            }
+        }
+        int resultAfter = after + other.after;
+        int resultBefor = result.size() - resultAfter + 1;
+        Collections.reverse(result);
+        result.add(0,sign);
+
+        return new FractionalNumber(resultBefor, resultAfter, result);
+    }
+
+    public FractionalNumber rounding(int order) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> thisResult = new ArrayList<>();
+        if (after <= order) {
+            numberUnion.add(0, sign);
+            return new FractionalNumber(numberUnion.size() - after, after, numberUnion);
+        } else {
+            thisResult.addAll(numberUnion.subList(0, before + order + 1));
+            int resultBefore;
+            int resultAfter;
+            if (numberUnion.get(before + order) >= 5) {
+                resultBefore = 1;
+                resultAfter = order;
+                result.add(1);
+                for (int i = order; i > 0; i--) {
+                    result.add(0, 0);
+                }
+                return new FractionalNumber(before, order, thisResult).plus(new FractionalNumber(resultBefore, resultAfter, result));
+            } else {
+                resultBefore = before;
+                resultAfter = order;
+                result.addAll(numberUnion.subList(0, before + order + 1));
+                return new FractionalNumber(resultBefore, resultAfter, result);
+            }
+        }
+    }
+
+    public FractionalNumber(double n){
+        this(Double.toString(n));
+    }
+
+    public double toDouble() {
+        double result = 0.0;
+        for (int i = 1; i <= before; i++) {
+            result = result + numberUnion.get(i - 1) * Math.pow(10, before - i);
+        }
+        for (int i = 1; i <= after; i++) {
+            result = result + (double) numberUnion.get(i + before - 1) / (double) Math.pow(10, i);
+        }
+
+        return result;
+    }
 
     @Override
     public String toString() {
@@ -316,7 +442,7 @@ public final class Number {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Number number = (Number) o;
+        FractionalNumber number = (FractionalNumber) o;
 
         if (before != number.before) return false;
         if (after != number.after) return false;
---
+++ test/FractionalNumberTest.java
@@ -0,0 +1,89 @@
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class FractionalNumberTest {
+
+
+    @Test
+    public void testToString() throws Exception {
+        List<Integer> numberUnion = Arrays.asList(0, -1, 8, 9, 2, 1);
+        assertEquals("-189.21", new FractionalNumber(4, 2, numberUnion).toString());
+    }
+
+    @Test
+    public void plus() {
+        List<Integer> numberUnion = new ArrayList<>();
+        List<Integer> otherNumberUnion = new ArrayList<>();
+        Collections.addAll(numberUnion, -1, 3, 9);
+        Collections.addAll(otherNumberUnion, 1, 3, 9);
+        assertEquals("-0.6",(new FractionalNumber("-0.9").plus(new FractionalNumber("0.3"))).toString());
+        assertEquals("-1.2",(new FractionalNumber("-0.9").plus(new FractionalNumber("-0.3"))).toString());
+        assertEquals("-1.0", (new FractionalNumber("-0.6").plus(new FractionalNumber("-0.4"))).toString());
+        assertEquals("00.0", (new FractionalNumber(2, 1, numberUnion).plus(new FractionalNumber(2, 1, otherNumberUnion))).toString());
+        assertEquals("110.50", (new FractionalNumber("55.45").plus(new FractionalNumber("55.05"))).toString());
+        assertEquals("1118888.9999",(new FractionalNumber("1000000.101").plus(new FractionalNumber("118888.8989"))).toString());
+        assertEquals("-30.9", (new FractionalNumber("-15.6").plus(new FractionalNumber("-15.3"))).toString());
+        assertEquals("30.9", (new FractionalNumber("15.6").plus(new FractionalNumber("15.3"))).toString());
+        assertEquals("0.9", (new FractionalNumber("0.6").plus(new FractionalNumber("0.3"))).toString());
+        assertEquals("1.0", (new FractionalNumber("0.6").plus(new FractionalNumber("0.4"))).toString());
+        assertEquals("-1.0", (new FractionalNumber("-0.6").plus(new FractionalNumber("-0.4"))).toString());
+        assertEquals("1.2",(new FractionalNumber("0.9").plus(new FractionalNumber("0.3"))).toString());
+        assertEquals("0.6",(new FractionalNumber("0.9").plus(new FractionalNumber("-0.3"))).toString());
+    }
+
+    @Test
+    public void minus() {
+        List<Integer> numberUnion = new ArrayList<>();
+        List<Integer> otherNumberUnion = new ArrayList<>();
+        Collections.addAll(numberUnion, -1, 3);
+        Collections.addAll(otherNumberUnion, -1, 5);
+        assertEquals("-1.0", (new FractionalNumber("-0.6").minus(new FractionalNumber("0.4"))).toString());
+        assertEquals("0.2", (new FractionalNumber(1, 1, numberUnion).minus(new FractionalNumber(1, 1, otherNumberUnion))).toString());
+        assertEquals("-3.0", (new FractionalNumber("5.9").minus(new FractionalNumber("8.9"))).toString());
+        assertEquals("0.0", (new FractionalNumber("8.9").minus(new FractionalNumber("8.9"))).toString());
+        assertEquals("-14.8", (new FractionalNumber("-5.9").minus(new FractionalNumber("8.9"))).toString());
+        assertEquals("-3.0", (new FractionalNumber("5.9").minus(new FractionalNumber("8.9"))).toString());
+        assertEquals("-0.3",(new FractionalNumber("-0.9").minus(new FractionalNumber("-0.6"))).toString());
+        assertEquals("1.5",(new FractionalNumber("0.9").minus(new FractionalNumber("-0.6"))).toString());
+        assertEquals("-0.3",(new FractionalNumber("-0.9").minus(new FractionalNumber("-0.6"))).toString());
+    }
+
+    @Test
+    public void multiplication(){
+        assertEquals("00.09",(new FractionalNumber("-0.3").multiplication(new FractionalNumber("-0.3"))).toString());
+        assertEquals("09.00",(new FractionalNumber("-3.0").multiplication(new FractionalNumber("-3.0"))).toString());
+        assertEquals("0121.00",(new FractionalNumber("11.0").multiplication(new FractionalNumber("11.0"))).toString());
+        assertEquals("1089.00",(new FractionalNumber("33.0").multiplication(new FractionalNumber("33.0"))).toString());
+        assertEquals("36.00",(new FractionalNumber("6.0").multiplication(new FractionalNumber("6.0"))).toString());
+        assertEquals("1089.00",(new FractionalNumber("33.0").multiplication(new FractionalNumber("33.0"))).toString());
+        assertEquals("099.00",(new FractionalNumber("33.0").multiplication(new FractionalNumber("3.0"))).toString());
+        assertEquals("-1089.00",(new FractionalNumber("33.0").multiplication(new FractionalNumber("-33.0"))).toString());
+        assertEquals("000.00",(new FractionalNumber("33.0").multiplication(new FractionalNumber("0.0"))).toString());
+        assertEquals("00.27",(new FractionalNumber("0.9").multiplication(new FractionalNumber("0.3"))).toString());
+        assertEquals("02.47",(new FractionalNumber("-1.9").multiplication(new FractionalNumber("-1.3"))).toString());
+    }
+
+    @Test
+    public void rounding(){
+        assertEquals("1.40", (new FractionalNumber("1.399").rounding(2).toString()));
+        assertEquals("1.39", (new FractionalNumber("1.386").rounding(2).toString()));
+        assertEquals("10.0", (new FractionalNumber("9.99").rounding(1).toString()));
+        assertEquals("10", (new FractionalNumber("10").rounding(2).toString()));
+        assertEquals("1.39", (new FractionalNumber("1.386").rounding(2).toString()));
+        assertEquals("10", (new FractionalNumber("9.9").rounding(0).toString()));
+        assertEquals("10", (new FractionalNumber("10").rounding(2).toString()));
+        assertEquals("10.0", (new FractionalNumber("9.99").rounding(1).toString()));
+        assertEquals("-10.0", (new FractionalNumber("-9.99").rounding(1).toString()));
+    }
+
+    @Test
+    public void toDouble() throws Exception{
+        assertEquals(9.99, new FractionalNumber("9.99").toDouble(), 1e-5);
+        assertEquals(0.123456789, new FractionalNumber("0.123456789").toDouble(), 1e-5);
+        assertEquals(0.99, new FractionalNumber("0.99").toDouble(), 1e-5);
+    }
+
+}
